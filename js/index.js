3f52edb5ed545ea6158d97a1bfee9436b16025e9
Aizhee <108914551+Aizhee@users.noreply.github.com>
2024-04-05 11:49:03 +0800

    added copy code feature

--- js/index.js
+++ js/index.js
@@ -236,12 +236,122 @@ function getRandomUserAgent() {
     return userAgent;
 }
 
-
 document.getElementById('updateLink').addEventListener('click', updateRepo);
 document.getElementById('shareURL').addEventListener('click', shareURL);
 document.getElementById('shareIFrame').addEventListener('click', shareIFrame);
-document.getElementById('copyCode').addEventListener('click', updateRepo);
+document.getElementById('copyCode').addEventListener('click', copyCode);
 document.getElementById('changeTheme').addEventListener('click', changeTheme);
+
+function copyCode() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const repositoryURL = urlParams.get('url');
+    const theme = urlParams.get('theme');
+
+    if (theme !== null) {
+        var decoded = atob(input);
+        var compressed = new Uint8Array(decoded.length);
+        for (var i = 0; i < decoded.length; i++) {
+        compressed[i] = decoded.charCodeAt(i);
+        }
+        var decompressed = pako.inflate(compressed, { to: 'string' });
+        var themeProperties = decompressed.split('$');
+
+        var root = document.documentElement;
+        var variableNames = [
+        "--primary-color",
+        "--secondary-color",
+        "--background-color-property",
+        "--background-transparency",
+        "--text-color-title",
+        "--text-color-primary",
+        "--text-color-secondary",
+        "--content-background-color",
+        "--content-background-color-alpha",
+        "--content-border-color",
+        "--content-border-radius",
+        "--content-border-width",
+        "--content-border-style",
+        "--content-max-width",
+        "--content-padding",
+        "--font-family-global",
+        "--margin-top-time",
+        "--font-size-title",
+        "--font-size-commit-message",
+        "--font-size-commit-time",
+        "--font-weight-title",
+        "--font-weight-commit-message",
+        "--font-weight-commit-time",
+        "--pointer-type",
+        "--branch-color",
+        "--branch-thickness",
+        "--branch-width",
+        "--ponter-size-arrow",
+        "--vertical-ruler-thickness",
+        "--vertical-ruler-color-start",
+        "--vertical-ruler-color-start-alpha",
+        "--vertical-ruler-color-end",
+        "--vertical-ruler-color-end-alpha",
+        "--dot-size",
+        "--dot-border-radius",
+        "--ponter-poition",
+        "--dot-box-shadow-color",
+        "--dot-box-shadow-color-alpha",
+        "--horizontal-shadow",
+        "--vertical-shadow",
+        "--blur-radius",
+        "--transition-property-seconds",
+        "--transition-property-bezier-curve",
+        "--hover-content-background-color",
+        "--hover-content-background-color-alpha",
+        "--hover-content-border-color",
+        "--content-text-gradient-background",
+        "--content-text-fill-color",
+        "--hover-content-border-style",
+        "--hover-content-border-radius",
+        "--hover-content-border-width",
+        "--hover-content-padding",
+        "--hover-box-shadow-color-main",
+        "--hover-box-shadow-color-main-alpha",
+        "--content-horizontal-shadow",
+        "--content-vertical-shadow",
+        "--content-blur-radius",
+        ];
+        var styleString = '';
+        variableNames.forEach(function(variableName, index) {
+            styleString += `${variableName}: ${themeProperties[index]}; `;
+    });
+
+    var StyleStr = `
+    <style>
+        :root {
+            ${styleString}
+        }
+    </style>
+    `;
+
+    } else {
+        var StyleStr = `<link rel="stylesheet" href="css/variables.css">`
+    }
+
+    navigator.clipboard.writeText(`
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Aizhee/Commit-Line/css/base-theme.css">
+        ` + StyleStr + `
+
+        <div id="commitLine">
+            <div id="timeline"></div>
+        </div>
+        
+        <script type="module">
+            generateCommitLine('`+ repositoryURL +`', "timeline", "commitLine", 20000)
+        </script>
+    `)
+    .then(() => {
+        alert("Successfully copied code");
+      })
+      .catch((e) => {
+        alert("Something went wrong",e);
+      });
+}
 
 function shareIFrame() {
     hideHeading()
