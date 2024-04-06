27c51caf469c37d653844790a321abe501219183
Aizhee <108914551+Aizhee@users.noreply.github.com>
2024-04-06 18:09:13 +0800

    share theme feature

--- js/designer.js
+++ js/designer.js
@@ -94,7 +94,7 @@ function constructCommitTimeline(commits) {
             container.className = index % 2 === 0 ? 'container left '+ pointerType : 'container right ' + pointerType;
 
             const content = document.createElement('div');
-            content.className = 'content';
+            content.className = index === 0 ? 'chover content' : 'content ';
 
             // Extracting commit message and timestamp
             const message = commit.commit.message;
@@ -109,11 +109,11 @@ function constructCommitTimeline(commits) {
 
             // Constructing content with message and timestamp
             const messageParagraph = document.createElement('p');
-            messageParagraph.className = 'messageText';
+            messageParagraph.className = index === 0 ? 'messageText choverp' : 'messageText';
             messageParagraph.textContent = message;
 
             const timestampParagraph = document.createElement('p');
-            timestampParagraph.className = 'timestampText';
+            timestampParagraph.className = index === 0 ? 'timestampText choverp' : 'timestampText';
             timestampParagraph.textContent = timestamp;
 
             content.appendChild(messageParagraph);
@@ -131,53 +131,11 @@ function constructCommitTimeline(commits) {
 
 document.addEventListener('DOMContentLoaded', function () {
     fetchCommitsInterval();
-    const inputFields = document.querySelectorAll('.input-color');
-    inputFields.forEach(inputField => {
-        createColorPicker(inputField);
-    });
 });
 
 
 
-// Function to create and position the color picker pseudo-element
-function createColorPicker(inputField) {
-    
-    const colorPicker = document.createElement('input');
-    colorPicker.type = 'color';
 
-    
-    colorPicker.style.position = 'absolute';
-    colorPicker.style.right = '5px'; 
-    colorPicker.style.top = '50%';
-    colorPicker.style.transform = 'translateY(-50%)';
-    colorPicker.style.height = '20px'; 
-
-    
-    colorPicker.addEventListener('change', function() {
-        const hexColor = colorPicker.value; 
-        const rgbColor = hexToRgb(hexColor); 
-        inputField.value = rgbColor; 
-    });
-
-    
-    // Get the label element associated with the input field
-    const label = inputField.parentNode.querySelector('label');
-    // Append the color picker to the label
-   
-    label.appendChild(colorPicker);
-}
-
-
-function hexToRgb(hex) {
-    
-    hex = hex.replace('#', '');
-    
-    const r = parseInt(hex.substring(0, 2), 16);
-    const g = parseInt(hex.substring(2, 4), 16);
-    const b = parseInt(hex.substring(4, 6), 16);
-    
-    return `(${r},${g},${b})`;
-}
 
 
 
